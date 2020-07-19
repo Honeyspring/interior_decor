5d52fd5f602c98557b840efa7c259cb07916adb3
unknown <emmanueldmlr@gmail.com>
2020-07-19 23:06:26 +0100

    Updated Send mail module and worked on captcha system

--- contact.php
+++ contact.php
@@ -24,6 +24,8 @@
     <link href="css/customize.css?" rel="stylesheet" />
     <script src="plugin/modernizr.js?"></script>
     <script src="../../vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js?"></script>
+    <script src='https://www.google.com/recaptcha/api.js?'></script>
+
 
 </head>
 
@@ -116,6 +118,9 @@
                                     <div class="col-md-12" id="submitInput">
                                         <input id="message_contact" name="message" type="text" class="form-control" placeholder="Message" required>
                                     </div>
+                                    <div class="col-md-4 col-lg-offset-4">
+                                        <div class="vertical-content mb-4 g-recaptcha" data-sitekey="6LeONGEUAAAAADTYEZQng4KrpP6B5GxO8Ek4WoGR" required>&nbsp;</div>
+                                    </div>
                                     <div class="col-md-12 text-center">
                                         <button type="submit" class="vk-btn">Send</button>
                                     </div>
@@ -150,22 +155,34 @@
     <script src="plugin/main.js?"></script>
     <script src="plugin/custom.js?"></script>
     <!-- END: SCRIPT -->
-    <?php
-    if (isset($_COOKIE["result"])){
-        echo ('Set');
-    ?>
-        <script type="text/javascript">
-            $('#submitInput').after('<div style="color: green; text-align: center"><h3 style="color: green;">Form submitted successfully</h3></div>');
-        </script>
-    <?
-    }
-    else{
-    ?>
-       <!-- <script type="text/javascript">
-        $('#submitInput').after('<div style="color: green; text-align: center"><h3 style="color: green;">Form submitted successfully</h3></div>');
-        </script>-->
-    <?php
-    }
-    ?>
+    <?php if(!isset($_COOKIE['result'])) {
+
+    } else {
+        if($_COOKIE['result'] == "The captcha field must be checked"){?>
+            <script type='text/javascript'>
+                alert('The captcha field must be checked');
+                $('#submitInput').after('<div class="mb-4" style="color: red; text-align: center">The captcha field must be checked</div>');
+            </script>
+        <?php  }
+        elseif ($_COOKIE['result'] == "All field must be filled") {?>
+            <script type='text/javascript'>
+                alert('All field must be filled');
+                $('#submitInput').after('<div class="mb-4" style="color: red; text-align: center">All field must be filled</div>');
+            </script>
+        <?php }
+        elseif ($_COOKIE['result'] == "Form submitted Successfully") {?>
+            <script type='text/javascript'>
+                alert('Form submitted successfully');
+                $('#submitInput').after('<div class="mb-4" style="color: green; text-align: center">Form submitted successfully</div>');
+            </script>
+        <?php }
+        else{
+        ?>
+            <script type='text/javascript'>
+                alert('Error Submitting the form');
+                $('#submitInput').after('<div class="mb-4" style="color: red; text-align: center">Error Submitting the form</div>');
+            </script>
+        <?php }
+    } ?>
 </body>
 </html>
--- mail/send-email.php
+++ mail/send-email.php
@@ -1,6 +1,6 @@
 <?php
 
-       $receipient = "emmanueldmlr@gmail.com";
+       /*$receipient = "emmanueldmlr@gmail.com";
         if(!isset($_POST["email"]) || !isset($_POST['phone_number']) || !isset($_POST['name'])|| !isset($_POST['message'])) {
             header('location:http://bracebrownn.com/contact.php');
         }
@@ -11,24 +11,20 @@
             $phone_number = $_POST['phone_number'];
             $message = $_POST['message'];
 
-            // Headers
+
             $headers = 'From: '.$name.' <'.$email.'>' . "\r\n";
             $headers .= 'Reply-To: '.$email.'' . "\r\n";
 
-            // Subject
+
             $subject = "New email from Interior Decoration Platform";
 
-            // Build Message
+
             $email_content = "First Name:$name\n";
             $email_content .= "Phone: $phone_number\n";
             $email_content .= "Message: $message \n\n";
             $email_content .= "Email: $email \n\n";
 
-
-// Check if sent
-
                 $sendmailResult = mail($receipient, $subject, $email_content, $headers);
-                /*die($sendmailResult);*/
                 if( $sendmailResult) {
                     $cookie_name = "result";
                     $cookie_value = "Form is submitted successfully";
@@ -41,4 +37,52 @@
                     header('location:http://bracebrownn.com/contact.php');
                 }
 
+        }*/
+if (empty($_POST["g-recaptcha-response"])) {
+    $cookie_name = "result";
+    $cookie_value = "The captcha field must be checked";
+    setcookie($cookie_name, $cookie_value, time() + (60 * 1), "/");
+    header('location:http://bracebrownn.com/contact.php');
+} else {
+    $receipient = "emmanueldmlr@gmail.com";
+
+    if(!isset($_POST["email"]) || !isset($_POST['phone_number']) || !isset($_POST['name'])|| !isset($_POST['message'])) {
+        $cookie_name = "result";
+        $cookie_value = "All field must be filled";
+        setcookie($cookie_name, $cookie_value, time() + (60 * 1), "/");
+        header('location:http://bracebrownn.com/contact.php');
+    } else {
+        $name = $_POST["name"];
+        $email = $_POST['email'];
+        $phone_number = $_POST['phone_number'];
+        $message = $_POST['message'];
+
+        $headers = 'From: '.$name.' <'.$email.'>' . "\r\n";
+        $headers .= 'Reply-To: '.$email.'' . "\r\n";
+
+
+        $subject = "New email from Interior Decoration Platform";
+
+
+        $email_content = "First Name:$name\n";
+        $email_content .= "Phone: $phone_number\n";
+        $email_content .= "Message: $message \n\n";
+        $email_content .= "Email: $email \n\n";
+
+
+        $sendmailResult = mail($receipient, $subject, $email_content, $headers);
+        if ($sendmailResult) {
+            $cookie_name = "result";
+            $cookie_value = "Form submitted Successfully";
+            setcookie($cookie_name, $cookie_value, time() + (60 * 1), "/");
+            header('location:http://bracebrownn.com/contact.php');
+        } else {
+            $cookie_name = "result";
+            $cookie_value = "Error Submitting the form";
+            setcookie($cookie_name, $cookie_value, time() + (60 * 1), "/");
+            header('location:http://bracebrownn.com/contact.php');
         }
+
+    }
+}
+
